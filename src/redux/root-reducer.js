eebf4df30cadb11ea3265ac611f8fe706d60454a
Rasila Hirani <darshanhirani@rasilas-MacBook-Air.local>
2021-12-08 19:37:20 +0000

    delete router folder

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,14 +1,11 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-//import authReducer from '../reducers/authReducer';
+
 import userReducer from "./user/user.reducer";
 import postReducer from './post/post.reducer';
 import filterReducer from './post-filter/post-filter.reducer'
-// import filterReducer from '../reducers/filterReducer';
-//import bookmarkReducer from '../reducers/bookmarkReducer';
 import bookmarkReducer from './bookmark/bookmark.reducer';
-//import commentReducer from '../reducers/commentReducer';
 import commentReducer from './comments/comment.reducer';
 const persistConfig ={
     key:'root',
--- src/routers/PublicRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Route} from 'react-router-dom';
-
-export const PublicRoute =(props) =>( 
-   <Route {...props} />    
-);
-
-
-export default PublicRoute;
